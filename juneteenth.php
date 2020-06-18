375216e64a1fb21f70e01c9caa9135f76ab26e8b
Alisa Herr <alisa@unitymakes.us>
2020-06-18 13:59:10 -0400

    Update version number

--- juneteenth.php
+++ juneteenth.php
@@ -9,7 +9,7 @@
  * Plugin Name:       Juneteenth Banner
  * Plugin URI:        https://github.com/unitymakesus/juneteenth
  * Description:       Spread Juneteenth awareness with this modal that automatically appears on your website on Juneteenth (WCAG 2.1 AA compliant). Links to https://sixnineteen.com/ for more information and to get involved.
- * Version:           1.0.0
+ * Version:           1.0.1
  * Author:            Unity Web Agency
  * Author URI:        https://unitywebagency.com/
  * License:           GPL-2.0+
@@ -28,7 +28,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'JUNETEENTH_VERSION', '1.0.0' );
+define( 'JUNETEENTH_VERSION', '1.0.1' );
 
 /**
  * The code that runs during plugin activation.
